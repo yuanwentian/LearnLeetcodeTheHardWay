51ad16c5c431526e7a0ec5be6e6e26a380bcca4c
yuanwentian <yuanwentian97@gmail.com>
2019-10-02 14:49:20 -0700

    modify 739Test & add 94

--- src/sol_739Test.java
+++ src/sol_739Test.java
@@ -9,7 +9,7 @@ public class sol_739Test {
     public void testdailyTemperatures() {
         int[] T = new int[]{73, 74, 75, 71, 69, 72, 76, 73};
         int[] expectedAns = new int[]{1, 1, 4, 2, 1, 1, 0, 0};
-        int[] actualAns = dT.dailyTemperatures(T);
+        int[] actualAns = dT.dailyTemperatures_method2(T);
         for (int i = 0; i < T.length; i++) {
             assertEquals(expectedAns[i], actualAns[i]);
         }
---
+++ src/sol_94.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+import java.util.ArrayList;
+import java.util.List;
+
+public class sol_94 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        helper(root, res);
+        return res;
+    }
+
+    public void helper(TreeNode root, List<Integer> res){
+        if (root != null) {
+            if (root.left != null) {
+                helper(root.left, res);
+            }
+            res.add(root.val);
+            if (root.right != null) {
+                helper(root.right, res);
+            }
+        }
+    }
+}
